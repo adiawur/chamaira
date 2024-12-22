6b5a09c208fe28eaab409668f3cf170e325507c4
aduawur <bitam10-092@suza.ac.tz>
2024-12-22 13:26:54 +0300

    my commit

---
+++ WordGame.java
@@ -0,0 +1,30 @@
+import java.util.scanner;
+public class WordGame{
+    public static void main(String[]args){
+        scanner r=new scanner(system.in);
+        String a=name;
+        String b=city;
+        String c=age;
+        String d=colege;
+        String e=profession;
+        String f=animal;
+        String g=petanimal;
+        System.out.println("please inter you name");
+        String name=r.nextline();
+        System.out.println("please inter your age");
+        Double age=r.nextdoube();
+        System.out.println("Please inter city name");
+        String city=r.nextline();
+        system.out.println("Pease inter colege name");
+        String colege=r.nextline();
+        system.out.println("please inter your profession");
+        String profession=r.nextline();
+        System.out.println("please inter animal name");
+        String anaima=r.nextline();
+        system.out.println("please inter pets name");
+        String petname=r.nextline();
+
+        System.out.println("there once was a person named 'a' who live in'b'.At the age of'c','a'went to colege at 'd'.'a'graduated and went to work as a 
+'e'.then,'a'adopted a'f' named'g'.They both ive happily ever after")
+    }
+}
