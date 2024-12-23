031996b4b8e97d3fcc1c3f8c2f591b0d7472d9d9
aduawur <bitam10-092@suza.ac.tz>
2024-12-23 07:34:23 +0300

    my commit

---
+++ Test.java
@@ -0,0 +1,22 @@
+public class Test{
+public static void main (String args[]){
+//int[] arr={10,20,30,};
+//for(int i=0;i<arr.length;i++) {
+//System.out.print(arr[i]+ "");
+//int[] arr={12,24,36};
+//int max=arr[0];
+//for(int num:arr){if (num>max)max=num;
+
+//System.out.println(arr.length);
+
+int[]arr={3,6,9};
+for(int i=0;i < arr.length;i++){
+	if (arr[i]%2==0){
+
+System.out.print(arr[1] + "");
+	}
+}
+
+
+}
+}
